a1c62ad0684cc81fe29dcea353704c57ef878d93
radek <radekpietrzak102@gmail.com>
2021-03-15 14:43:34 +0100

    TaskController GetMapping added

--- src/main/java/io/github/mat3e/controller/TaskController.java
+++ src/main/java/io/github/mat3e/controller/TaskController.java
@@ -1,9 +1,16 @@
 package io.github.mat3e.controller;
 
+import io.github.mat3e.model.Task;
 import io.github.mat3e.model.TaskRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.rest.webmvc.RepositoryRestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
+
 
 @RepositoryRestController
 public class TaskController {
@@ -12,6 +19,18 @@ public class TaskController {
 
     public TaskController(TaskRepository repository) {
         this.repository = repository;
+    }
+
+    @GetMapping(value = "/tasks", params = {"!sort", "!page", "!size"})
+    ResponseEntity<List<Task>> readAllTasks() {
+        logger.warn("Exposing all the tasks!");
+        return ResponseEntity.ok(repository.findAll());
+    }
+
+    @GetMapping("/tasks")
+    ResponseEntity<List<Task>> readAllTasks(Pageable page) {
+        logger.info("Custom pageable");
+        return ResponseEntity.ok(repository.findAll(page).getContent());
     }
 
 
