8d82f1d96fb2f5daedfe03807e9948838624ca25
radek <radekpietrzak102@gmail.com>
2021-03-16 13:31:11 +0100

    configuration info added

---
+++ src/main/java/io/github/mat3e/TaskConfigurationProperties.java
@@ -0,0 +1,18 @@
+package io.github.mat3e;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ConfigurationProperties("task")
+public class TaskConfigurationProperties {
+    private boolean allowMultipleTasksFromTemplate;
+
+    public boolean isAllowMultipleTasksFromTemplate() {
+        return allowMultipleTasksFromTemplate;
+    }
+
+    public void setAllowMultipleTasksFromTemplate(boolean allowMultipleTasksFromTemplate) {
+        this.allowMultipleTasksFromTemplate = allowMultipleTasksFromTemplate;
+    }
+}
--- src/main/java/io/github/mat3e/controller/InfoController.java
+++ src/main/java/io/github/mat3e/controller/InfoController.java
@@ -1,23 +1,29 @@
 package io.github.mat3e.controller;
 
+import io.github.mat3e.TaskConfigurationProperties;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceProperties;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class InfoController {
-    @Value("${spring.datasource.url")
-    private String url;
-    @Value("${myProp")
-    private String myProp;
+    private final DataSourceProperties dataSource;
+    private TaskConfigurationProperties myProp;
+
+    public InfoController(DataSourceProperties dataSource, TaskConfigurationProperties myProp) {
+        this.dataSource = dataSource;
+        this.myProp = myProp;
+    }
 
     @GetMapping("/info/url")
     String url() {
-        return url;
+        return dataSource.getUrl();
     }
 
     @GetMapping("/info/prop")
-    String myProp() {
-        return myProp;
+    boolean myProp() {
+        return myProp.isAllowMultipleTasksFromTemplate();
     }
 }
