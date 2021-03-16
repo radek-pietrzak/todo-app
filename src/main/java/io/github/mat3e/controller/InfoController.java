0b00abc2dd5995bdac6f240a9593dbc4b6ef6c0c
radek <radekpietrzak102@gmail.com>
2021-03-16 12:51:37 +0100

    properties management

---
+++ src/main/java/io/github/mat3e/controller/InfoController.java
@@ -0,0 +1,23 @@
+package io.github.mat3e.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class InfoController {
+    @Value("${spring.datasource.url")
+    private String url;
+    @Value("${myProp")
+    private String myProp;
+
+    @GetMapping("/info/url")
+    String url() {
+        return url;
+    }
+
+    @GetMapping("/info/prop")
+    String myProp() {
+        return myProp;
+    }
+}
