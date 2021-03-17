030b0a747721e664584077142b8f1936f612ca7e
radek <radekpietrzak102@gmail.com>
2021-03-17 11:41:09 +0100

    createdOn and updatedOn added to Task

--- src/main/java/io/github/mat3e/model/Task.java
+++ src/main/java/io/github/mat3e/model/Task.java
@@ -16,6 +16,8 @@ public class Task {
     private String description;
     private boolean done;
     private LocalDateTime deadline;
+    private LocalDateTime createdOn;
+    private LocalDateTime updatedOn;
 
     public Task() {
     }
